1d068760f204b7526dfbbd35dc79524badc838bb
korinneramcharitar <77815286+korinneramcharitar@users.noreply.github.com>
2021-06-06 20:57:56 -0400

    Update App.java

--- src/main/java/base/App.java
+++ src/main/java/base/App.java
@@ -1,3 +1,7 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Korinne Ramcharitar
+ */
 package base;
 //Mad libs are a simple game where you create a story template with blanks for words. You, or another player, then construct a list of words and place them into the story, creating an often silly or funny story as a result.
 //Create a simple mad-lib program that prompts for a noun, a verb, an adverb, and an adjective and injects those into a story that you create.
